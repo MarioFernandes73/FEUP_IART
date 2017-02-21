f3c99a8744cbd760ea4726c4e5dcfc0674bfad28
Mário Fernandes <mgustavofernandes@gmail.com>
2017-02-21 10:55:30 +0000

    project startup

---
+++ src/IART.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : IART.cpp
+// Author      : Catarina Ramos, Inês Gomes and Mário Fernandes
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
